1481553c5d1d9dd3a6863e15dc6f0b663822697d
codingtoc <codingtoc@gmail.com>
2024-12-20 17:44:37 +0900

    Career LWC Timeline UI로 변경

--- force-app/main/default/lwc/career/career.js
+++ force-app/main/default/lwc/career/career.js
@@ -1,54 +1,44 @@
-import CompanyName from "@salesforce/schema/User.CompanyName";
 import { LightningElement } from "lwc";
 
 export default class Career extends LightningElement {
-  columns = [
-    { label: "기간", fieldName: "Period" },
-    {
-      label: "회사",
-      fieldName: "CompanyUrl",
-      type: "url",
-      typeAttributes: { label: { fieldName: "CompanyName" }, target: "_blank" }
-    },
-    { label: "부서", fieldName: "Department" },
-    { label: "직위", fieldName: "Title" }
-  ];
-
-  rows = [
-    {
-      Period: "2022.12 ~ 2024.07",
-      CompanyUrl: "https://www.dkbmc.com",
-      CompanyName: "디케이비엠시",
-      Department: "Salesforce CRM 컨설팅팀",
-      Title: "부장"
-    },
-    {
-      Period: "2019.03 ~ 2022.07",
-      CompanyUrl: "https://www.douzone.com",
-      CompanyName: "더존비즈온",
-      Department: "SCM설계팀",
-      Title: "부장"
-    },
-    {
-      Period: "2010.02 ~ 2018.08",
-      CompanyUrl: "https://www.douzone.com",
-      CompanyName: "더존비즈온",
-      Department: "ERP설계팀",
-      Title: "부장"
-    },
-    {
-      Period: "2003.06 ~ 2008.12",
-      CompanyUrl: "https://www.douzone.com",
-      CompanyName: "더존홀딩스",
-      Department: "ERP컨설팅팀",
-      Title: "과장"
-    },
-    {
-      Period: "2001.12 ~ 2003.06",
-      CompanyUrl: "https://www.douzone.com",
-      CompanyName: "더존디지털웨어",
-      Department: "ERP컨설팅팀",
-      Title: "사원"
-    }
-  ];
+  workExperienceData = {
+    heading: "Career History",
+    workExperiences: [
+      {
+        role: "Salesforce CRM 컨설팅팀 부장",
+        companyName: "디케이비엠시",
+        companyUrl: "https://www.dkbmc.com",
+        duration: "2022.12 ~ 2024.07",
+        description: "Salesforce CRM 분석, 설계, 개발, 컨설팅"
+      },
+      {
+        role: "SCM설계팀 부장",
+        companyName: "더존비즈온",
+        companyUrl: "https://www.douzone.com",
+        duration: "2019.03 ~ 2022.07",
+        description: "ERP SCM 모듈 분석, 설계, 컨설팅"
+      },
+      {
+        role: "ERP설계팀 부장",
+        companyName: "더존비즈온",
+        companyUrl: "https://www.douzone.com",
+        duration: "2010.02 ~ 2018.08",
+        description: "ERP 영업/구매/재고/생산/원가 모듈 분석, 설계, 컨설팅"
+      },
+      {
+        role: "ERP컨설팅팀 과장",
+        companyName: "더존홀딩스",
+        companyUrl: "https://www.douzone.com",
+        duration: "2003.06 ~ 2008.12",
+        description: "ERP 영업/구매/재고/생산/원가 모듈 분석, 설계, 컨설팅"
+      },
+      {
+        role: "ERP컨설팅팀 사원",
+        companyName: "더존디지털웨어",
+        companyUrl: "https://www.douzone.com",
+        duration: "2001.12 ~ 2003.06",
+        description: "ERP 영업/구매/재고 모듈 분석, 설계, 컨설팅"
+      }
+    ]
+  };
 }
